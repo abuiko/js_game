abc2b4b13be5112de2c4830e38f708d6da087578
Alena Buiko <abuiko.ma@gmail.com>
2022-08-10 22:59:39 -0400

    adjust animation loop

--- script.js
+++ script.js
@@ -7,11 +7,44 @@ const playerImage = new Image()
 playerImage.src = '/assets/shadow_dog.png'
 const spriteWidth = 575
 const spriteHeight = 523
+let frameX = 0
+let frameY = 0
+let gameFrame = 0
+const staggerFrames = 5
+const spriteAnimations = []
+const animationStates = [
+    {
+        name: 'idle',
+        frames: 7,
+    },
+    {
+        name: 'jump',
+        frames: 7,
+    }
+]
+
+animationStates.forEach((state, index) => {
+    let frames = {
+        loc: [],
+    }
+
+    for (let i = 0; i < state.frames; i++) {
+        let positionX = i * spriteWidth
+        let positionY = index * spriteHeight
+        frames.loc.push({ x: positionX, y: positionY })
+    }
+    spriteAnimations[state.name] = frames
+})
+
+console.log(spriteAnimations)
 
 function animate() {
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
-    // ctx.fillRect(50, 50, 100, 100)
-    ctx.drawImage(playerImage, 0, 0, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight)
+    let position = Math.floor(gameFrame / staggerFrames) % spriteAnimations['idle'].loc.length
+    frameX = spriteWidth * position
+    ctx.drawImage(playerImage, frameX, frameY * spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight)
+
+    gameFrame++
     requestAnimationFrame(animate)
 }
 
